180ea243ef47085f9894225c35c38d3e0d056a1b
xutaoding <xutao.ding@chinascopefinancial.com>
2016-08-22 10:49:42 +0800

    ...

--- jobs/clean.py
+++ jobs/clean.py
@@ -2,7 +2,7 @@
 # author: shuqing.zhou
 import sys
 from os.path import dirname, abspath
-from datetime import datetime, date
+from datetime import datetime, date, timedelta
 from pymongo import MongoClient
 
 sys.path.append(dirname(dirname(abspath(__file__))))
@@ -11,13 +11,13 @@ from config.config_otc import *
 from eggs.utils.utils import md5
 
 
-def clean_replica():
+def clean_replica(days=0):
     client = MongoClient(DATA_HOST)
     coll = client[DB_OTC][TABLE_OTC]
 
     urls = set()
     fields = {"sid": 1}
-    query = {"sid": {"$regex": "http"}, 'pub': str(date.today())}
+    query = {"sid": {"$regex": "http"}, 'pub': str(date.today() - timedelta(days=days))}
     for item in coll.find(query, fields):
         sid_md5 = md5(item["sid"])
 
